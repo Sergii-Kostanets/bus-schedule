358d915dc6891a7ae123be86b04246e53621f410
Sergii Kostanets <sergiikostanets@gmail.com>
2024-06-07 12:07:05 +0100

    netlify

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-const sheetId = sheetIdSecret();
-const apiKey = apiKeySecret();
+// GitHub
+// const sheetId = sheetIdSecret();
+// const apiKey = apiKeySecret();
+
+// Netlify
+const sheetId = process.env.SHEET_ID;
+const apiKey = process.env.API_KEY;
+
 const range = '427 G->T!D1:D25';
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
 
