7e319cbbbbe4ef4d439213892bba96b1279225d8
Sergii Kostanets <sergiikostanets@gmail.com>
2024-06-07 14:20:34 +0100

    add days, delete old layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,51 @@
 const sheetId = "1774H66Bt1Gl9MT_YLxuFpbDZtzcPe4XQgjB1p9Eiovo";
 const apiKey = "AIzaSyD8XLZMEgRsPCeKzo5aZ0eSrN7XolPrJhQ";
 
+const bus427GalwayTuamSchedule = '427 G->T!A1:A25';
 const bus427GalwayTuamGalwayATU = '427 G->T!B1:B25';
 const bus427GalwayTuamGalwayCity = '427 G->T!C1:C25';
 const bus427GalwayTuamClaregalway = '427 G->T!D1:D25';
 
+let schedule = bus427GalwayTuamSchedule;
 let departure = bus427GalwayTuamGalwayCity;
 let arrival = bus427GalwayTuamClaregalway;
 
+const urlSchedule = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${schedule}?key=${apiKey}`;
 const urlDeparture = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${departure}?key=${apiKey}`;
 const urlArrival = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${arrival}?key=${apiKey}`;
 
-fetch(urlDeparture)
+fetch(urlSchedule)
     .then(response => response.json())
     .then(data => {
-        const departure = document.getElementById('departure');
+        const schedule = document.getElementById('schedule-days');
         data.values.forEach(row => {
           const rowElement = document.createElement('div');
           rowElement.textContent = row.join(', ');
-          departure.appendChild(rowElement);
+          schedule.appendChild(rowElement);
         });
     })
     .catch(error => console.error('Error fetching data:', error));
 
-    fetch(urlArrival)
+    fetch(urlDeparture)
         .then(response => response.json())
         .then(data => {
-            const arrival = document.getElementById('arrival');
+            const departure = document.getElementById('departure');
             data.values.forEach(row => {
               const rowElement = document.createElement('div');
               rowElement.textContent = row.join(', ');
-              arrival.appendChild(rowElement);
+              departure.appendChild(rowElement);
             });
         })
         .catch(error => console.error('Error fetching data:', error));
+
+fetch(urlArrival)
+    .then(response => response.json())
+    .then(data => {
+        const arrival = document.getElementById('arrival');
+        data.values.forEach(row => {
+            const rowElement = document.createElement('div');
+            rowElement.textContent = row.join(', ');
+            arrival.appendChild(rowElement);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
