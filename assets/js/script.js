f7003cbfdb7ea93c6397c62e1a7621f65d22fd38
Sergii-Kostanets <98876306+Sergii-Kostanets@users.noreply.github.com>
2025-11-10 18:09:01 +0000

    add 419 route

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,14 @@ function populateOptions(routeValue) {
 
     // Determine the range based on the selected route
     let range, defaultDeparture;
+    if (routeValue === '419-Galway->Moycullen') {
+        range = '419-Galway->Moycullen!B1:D1';
+        defaultDeparture = 'Galway City';
+        defaultArrival = 'Moycullen';
+    if (routeValue === '419-Moycullen->Galway') {
+        range = '419-Moycullen->Galway!B1:D1';
+        defaultDeparture = 'Moycullen';
+        defaultArrival = 'Galway City';
     if (routeValue === '427-Galway->Tuam') {
         range = '427-Galway->Tuam!B1:J1';
         defaultDeparture = 'Galway City';
